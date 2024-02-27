ab483b81a5da265b6ac75f28c627e1fb69813839
gentlepuck071 <passion.puck111@gmail.com>
2024-02-27 19:02:30 +0100

    feature: added a moving square

--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,54 @@
 #include <SDL.h>
-#include <iostream>
 
-int main(void) {
-    // Init SDL
-    SDL_Init(SDL_INIT_VIDEO);
+// Constants
+const int SCREEN_WIDTH = 800;
+const int SCREEN_HEIGHT = 600;
+const int SQUARE_SIZE = 50;
+
+// Function to handle events
+void handleEvents(SDL_Event& e, bool& quit, int& posX, int& posY);
+
+int main(int argc, char* args[]) {
+    // Initialize SDL
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
+        return 1;
+    }
 
     // Create a window
-    SDL_Window* window = SDL_CreateWindow("PACMAN", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 800, 600, SDL_WINDOW_SHOWN);
+    SDL_Window* window = SDL_CreateWindow("Moving Square", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+    if (!window) {
+        printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
+        return 1;
+    }
 
     // Create a renderer
     SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-    
+    if (!renderer) {
+        printf("Renderer could not be created! SDL_Error: %s\n", SDL_GetError());
+        return 1;
+    }
+
+    // Initial position of the square
+    int posX = SCREEN_WIDTH / 2 - SQUARE_SIZE / 2;
+    int posY = SCREEN_HEIGHT / 2 - SQUARE_SIZE / 2;
+
     // Game loop
     bool quit = false;
+    SDL_Event e;
+
     while (!quit) {
-        // Handle events
-        SDL_Event e;
-        while (SDL_PollEvent(&e) != 0) {
-            if (e.type == SDL_QUIT) {
-                quit = true;
-            }
-        }
+        // Event handling
+        handleEvents(e, quit, posX, posY);
 
         // Clear the screen
         SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
         SDL_RenderClear(renderer);
 
-        // Draw game elements here
+        // Draw the moving square
+        SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+        SDL_Rect squareRect = { posX, posY, SQUARE_SIZE, SQUARE_SIZE };
+        SDL_RenderFillRect(renderer, &squareRect);
 
         // Update the screen
         SDL_RenderPresent(renderer);
@@ -39,3 +61,27 @@ int main(void) {
 
     return 0;
 }
+
+void handleEvents(SDL_Event& e, bool& quit, int& posX, int& posY) {
+    while (SDL_PollEvent(&e) != 0) {
+        if (e.type == SDL_QUIT) {
+            quit = true;
+        } else if (e.type == SDL_KEYDOWN) {
+            switch (e.key.keysym.sym) {
+                case SDLK_UP:
+                    posY -= 10;
+                    break;
+                case SDLK_DOWN:
+                    posY += 10;
+                    break;
+                case SDLK_LEFT:
+                    posX -= 10;
+                    break;
+                case SDLK_RIGHT:
+                    posX += 10;
+                    break;
+            }
+        }
+    }
+}
+
